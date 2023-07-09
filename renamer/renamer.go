66ec53ddb962843b3a2fcb590b4b02c51ec14f59
Shawn Ma <shawnma@gmail.com>
2023-07-09 09:55:15 -0700

    Adding support for overriding and a sequence number

--- renamer/renamer.go
+++ renamer/renamer.go
@@ -3,37 +3,17 @@ package main
 import (
 	"fmt"
 	"io/fs"
-	"log"
+
 	"os"
 	"path"
 	"path/filepath"
 	"strings"
 	"time"
 
-	flag "github.com/spf13/pflag"
-
 	"github.com/evanoberholster/imagemeta"
-	"github.com/fatih/color"
+	flag "github.com/spf13/pflag"
+	"shawnma.com/pic_rename/log"
 )
-
-func main() {
-	monthOnly := flag.BoolP("month-only", "m", false, "use month in path only")
-	dest := flag.StringP("dest", "d", "output", "Destination dir")
-	src := flag.StringP("src", "s", "", "source dir")
-	flag.Parse()
-	if src == nil || *src == "" {
-		fmt.Printf("%s: required --src\n", os.Args[0])
-		flag.PrintDefaults()
-		return
-	}
-	r := renamer{
-		dirsCache: make(map[string]bool),
-		monthOnly: *monthOnly,
-		src:       *src,
-		dest:      *dest,
-	}
-	r.rename()
-}
 
 const (
 	nameFormat      = "20060102_150405"
@@ -41,42 +21,71 @@ const (
 	folderMonthOnly = "2006.01"
 )
 
+var logger = log.New()
+
 type renamer struct {
 	dirsCache map[string]bool
 	monthOnly bool
+	overwrite bool
+	seq       bool // whether to rename the files using a seq number if already exists
 	dest      string
 	src       string
 }
 
+func (r *renamer) parseFlags() {
+	flag.BoolVarP(&r.monthOnly, "month-only", "m", false, "use month in path only")
+	flag.BoolVarP(&r.overwrite, "overwrite", "o", false, "overwrite the file if the file exists")
+	flag.BoolVarP(&r.seq, "seq", "q", false, "if the dest exits, using a seq number suffix")
+
+	flag.StringVarP(&r.dest, "dest", "d", "output", "destination dir")
+	flag.StringVarP(&r.src, "src", "s", "", "source dir")
+	flag.Parse()
+}
+
 func (r *renamer) rename() {
-	log.Printf("Renaming pictures, src=%s, dest=%s, month only=%v\n", r.src, r.dest, r.monthOnly)
+	logger.Info("Renaming pictures, src=%s, dest=%s, month only=%v\n", r.src, r.dest, r.monthOnly)
 	filepath.Walk(r.src, func(path string, info fs.FileInfo, err error) error {
 		if !info.IsDir() {
 			suffix := strings.ToLower(filepath.Ext(path))
 			if suffix == ".jpg" || suffix == ".heic" || suffix == ".cr2" || suffix == ".jpeg" {
-				t, e := getDate(path)
-				if e != nil || t.Second() == 0 {
-					color.Red("Error getting date from %s: %v, t=%q\n", path, e, t)
-				} else {
-					dest := r.getDest(t, suffix)
-					if _, err := os.Stat(dest); err == nil {
-						color.HiRed("File %s already exists", dest)
-						return nil
-					}
-					e = os.Rename(path, dest)
-					if e != nil {
-						color.Red("Failed to rename %s->%s: %s", path, dest, e)
-					} else {
-						color.Cyan("Success: %s->%s", path, dest)
-					}
-				}
+				r.renameOne(path, suffix)
 			}
 		}
 		return nil
 	})
 }
 
-func (r *renamer) getDest(t time.Time, suffix string) string {
+func (r *renamer) renameOne(path string, suffix string) {
+	t, e := getDate(path)
+	if e != nil || t.Year() < 2000 {
+		logger.Error("Error getting date from %s: %v, t=%q\n", path, e, t)
+		return
+	}
+
+	for i := 0; ; i++ {
+		dest := r.getDest(t, suffix, i)
+		if _, err := os.Stat(dest); err == nil {
+			logger.Info("%s -> %s: File already exists", path, dest)
+			if r.seq {
+				// get next file name
+				logger.Info("Getting next file name")
+				continue
+			}
+			if !r.overwrite {
+				return
+			}
+		}
+		e = os.Rename(path, dest)
+		if e != nil {
+			logger.Error("Failed to rename %s->%s: %s", path, dest, e)
+		} else {
+			logger.Debug("Success: %s->%s", path, dest)
+		}
+		return
+	}
+}
+
+func (r *renamer) getDest(t time.Time, suffix string, seq int) string {
 	var folder string
 	if r.monthOnly {
 		folder = t.Format(folderMonthOnly)
@@ -88,7 +97,12 @@ func (r *renamer) getDest(t time.Time, suffix string) string {
 		os.MkdirAll(destDir, 0700)
 		r.dirsCache[destDir] = true
 	}
-	file := t.Format(nameFormat) + suffix
+	var file string
+	if seq == 0 {
+		file = t.Format(nameFormat) + suffix
+	} else {
+		file = fmt.Sprintf("%s_%d%s", t.Format(nameFormat), seq, suffix)
+	}
 	return path.Join(destDir, file)
 }
 
@@ -103,4 +117,17 @@ func getDate(path string) (time.Time, error) {
 		return time.Time{}, err
 	}
 	return e.CreateDate(), nil
+}
+
+func main() {
+	r := renamer{
+		dirsCache: make(map[string]bool),
+	}
+	r.parseFlags()
+	if r.src == "" {
+		fmt.Printf("%s: required --src\n", os.Args[0])
+		flag.PrintDefaults()
+		return
+	}
+	r.rename()
 }
